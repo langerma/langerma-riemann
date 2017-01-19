01a5970ae3f40694e2e6dc7f98764ee73eebe48e
Markus Langer <langer.markus@gmail.com>
2017-01-19 17:35:24 +0100

    added logstash events and disabled icinga graphite parser

--- riemann.clj
+++ riemann.clj
@@ -9,24 +9,32 @@
   (tcp-server {:host host})
   (udp-server {:host host})
   (ws-server  {:host host})
-  ;(graphite-server {:host host})
-  (graphite-server :host host
-                   :parser-fn
-                   (fn [{:keys [service] :as event}]
-                     (if-let [[source hostname checktype servicename checkname perfdata metricname] (clojure.string/split service #"\." 7)]
-                       (if-let [[servicetype realservicename] (clojure.string/split servicename #"_" 2)]
-                        {
-                          :host (clojure.string/replace hostname #"_" ".")
-                          :service (clojure.string/join "." [servicetype realservicename metricname])
-                          :metric (:metric event)
-                          :tags source
-                          :checktype checktype
-                          :state "ok"
-                          :servicetype servicetype
-                          :time (:time event)
-                          :ttl 30}))))
+  (sse-server {:host host})
+  (graphite-server {:host host})
+  ;(graphite-server :host host
+  ;                 :parser-fn
+  ;                 (fn [{:keys [service] :as event}]
+  ;                   (if-let [[source hostname checktype servicename checkname perfdata metricname] (clojure.string/split service #"\." 7)]
+  ;                    (if-let [[servicetype realservicename] (clojure.string/split servicename #"_" 2)]
+  ;                      {
+  ;                        :host (clojure.string/replace hostname #"_" ".")
+  ;                        :service (clojure.string/join "." [servicetype realservicename metricname])
+  ;                        :metric (:metric event)
+  ;                        :tags source
+  ;                        :checktype checktype
+  ;                        :state "ok"
+  ;                        :servicetype servicetype
+  ;                        :time (:time event)
+  ;                        :ttl 30}))))
   (def graph
-    (opentsdb {:host host})))
+    (opentsdb {:host host}))
+  (def logstash-forward
+    (logstash {:host host
+               :port 25827
+               :pool-size 4
+               :protocol :tcp
+               :claim-timeout 0.1
+               :reconnect-interval 4 })))
 
 (periodically-expire 30 {:keep-keys [:host :service :tags, :state, :description, :metric]})
 (let [index (index)]
@@ -37,6 +45,10 @@
       ;       graph)
       (where (tagged-any "collectd")
              graph)
+      (where-rewrite (host nil)
+             graph)
+      logstash-forward
+      ;#(info %)
       (expired
         (fn [event] (info "EXPIRED" event))))))
       ;#(info %))))
