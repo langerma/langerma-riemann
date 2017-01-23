f4795cf5b0f48bf28a1f6456533ea045a83f3a29
Markus Langer <langer.markus@gmail.com>
2017-01-23 08:01:57 +0100

    rewrote icinga graphite server parser

--- riemann.clj
+++ riemann.clj
@@ -3,6 +3,11 @@
 
 (logging/init {:file "/var/log/riemann/riemann.log"})
 
+(require '[clojure.string :as string])
+
+(include "inc/helpers.clj")
+(include "inc/thresholds.clj")
+
 ; Listen on the local interface over TCP (5555), UDP (5555), and websockets
 ; (5556)
 (let [host "0.0.0.0"]
@@ -10,22 +15,35 @@
   (udp-server {:host host})
   (ws-server  {:host host})
   (sse-server {:host host})
-  (graphite-server {:host host})
-  ;(graphite-server :host host
-  ;                 :parser-fn
-  ;                 (fn [{:keys [service] :as event}]
-  ;                   (if-let [[source hostname checktype servicename checkname perfdata metricname] (clojure.string/split service #"\." 7)]
-  ;                    (if-let [[servicetype realservicename] (clojure.string/split servicename #"_" 2)]
-  ;                      {
-  ;                        :host (clojure.string/replace hostname #"_" ".")
-  ;                        :service (clojure.string/join "." [servicetype realservicename metricname])
-  ;                        :metric (:metric event)
-  ;                        :tags source
-  ;                        :checktype checktype
-  ;                        :state "ok"
-  ;                        :servicetype servicetype
-  ;                        :time (:time event)
-  ;                        :ttl 30}))))
+
+  (graphite-server :host host
+                   :port 2004
+                   :parser-fn
+                   (fn [{:keys [service] :as event}]
+                     (if-let [[source hostname checktype servicename checkname perfdata metricname] (string/split service #"\." 7)]
+                      (if-let [[servicetype realservicename] (clojure.string/split servicename #"_" 2)]
+                        {
+                          :host (string/replace hostname #"_" ".")
+                          :service (string/join "." ["icinga" "riemann" servicetype realservicename (string/replace (string/replace metricname #"load_1_min|load_15_min|load_5_min" {"load_1_min" "load1" "load_15_min" "load15" "load_5_min" "load5"}) #"\.value" "")])
+                          :metric (:metric event)
+                          :tags source
+                          :checktype checktype
+                          :state "ok"
+                          :servicetype servicetype
+                          :time (:time event)
+                          :ttl 60}))))
+  (graphite-server :host host
+                   :parser-fn
+                   (fn [{:keys [service] :as event}]
+                     (if-let [[hostname metricname] (string/split service #"\." 2)]
+                        {
+                          :host "at-vie-fn01"
+                          :service (string/join "." ["freenas" (string/replace metricname #"\.value" "")])
+                          :metric (:metric event)
+                          :tags ["FreeNAS"]
+                          :state "ok"
+                          :time (:time event)
+                          :ttl 60})))
   (def graph
     (opentsdb {:host host}))
   (def logstash-forward
@@ -39,13 +57,15 @@
 ;(periodically-expire 30 {:keep-keys [:host :service :tags, :state, :description, :metric]})
 (let [index (index)]
   (streams
-    (default :ttl 30
+    (default :ttl 60
       index
-      ;(where (tags "icinga2")
-      ;       graph)
-      (where (tagged-any "collectd")
+      (where (tags "icinga2")
+             ;#(info %)
              graph)
-      (where (host nil)
+      (where (tagged-any "FreeNAS")
+             ;#(info %)
+             graph)
+      (where (tagged-any "collectd")
              graph)
       logstash-forward
       ;#(info %)
